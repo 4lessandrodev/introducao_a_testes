d54ad101da1c0e1dd742dbd7e3366ba217885445
Alessandro Menezes <alessandroadm@live.com>
2020-06-22 21:02:27 -0300

    alguns testes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,9 @@
 module.exports = {
   
-  somar(n1, n2){
-    return n1 + n2;
+  somar(...params) {
+    return params.reduce((total, num) => {
+      return parseFloat(total) + parseFloat(num);
+    },0);
   },
 
   multiplicar(n1, n2){
--- tests/User.test.js
+++ tests/User.test.js
@@ -6,3 +6,17 @@ test('Somar dois valores', () => {
   expect(UserController.somar(1, 2)).toBeDefined();
   expect(UserController.somar(1, 2)).not.toBeUndefined();
 });
+
+test('Somar dois valores sendo 1 dos valores zero', () => {
+  expect(UserController.somar(0, 2)).toBe(2);
+  expect(UserController.somar(1, 2)).not.toBeNull();
+  expect(UserController.somar(1, 2)).toBeDefined();
+  expect(UserController.somar(1, 2)).not.toBeUndefined();
+});
+
+test('Somar passando apenas um valor', () => {
+  expect(UserController.somar(2)).toBe(2);
+  expect(UserController.somar(1, 2)).not.toBeNull();
+  expect(UserController.somar(1, 2)).toBeDefined();
+  expect(UserController.somar(1, 2)).not.toBeUndefined();
+});
